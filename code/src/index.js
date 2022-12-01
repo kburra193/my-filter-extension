5695d05cee7dab4c416b507168b4bc4b69a445ab
Karthik Burra <kburra193@gmail.com>
2022-11-30 19:35:20 -0600

    Boiler-Plate Extension Set up

---
+++ code/src/index.js
@@ -0,0 +1,13 @@
+import { initialProperties, template, definition, controller, paint, resize } from "./methods"
+import "./style.css"
+
+window.define([], function() {
+	return {
+		initialProperties,
+		template,
+		definition,
+		controller,
+		paint,
+		resize,
+	}
+})
