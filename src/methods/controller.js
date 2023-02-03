c7363c3bec287f9333de6c2016e7d91d016a5a91
KB <101477932+kburra193@users.noreply.github.com>
2023-02-03 14:59:08 -0600

    Update controller.js

--- src/methods/controller.js
+++ src/methods/controller.js
@@ -8,20 +8,57 @@ export default [
     // Toggle Dropdown
     $scope.onDropdownToggleClick = function (event) {
       if ($scope.mode != "analysis") return;
-        else {
-        //Close open popovers
-        var popover = $(event.target).parent().find(".listbox");
-        $(document.body)
-          .find(".listbox")
-          .each((_, item) => {
-            !$(item).is(popover) ? $(item).removeClass("active") : "";
-          });
-          if (popover.hasClass("active")) {
-          popover.removeClass("active");
+      //Close open popovers
+      var $popover = $element.find(".listbox.lui-popover");
+      var $dropdown = $element.find(".dropdown-toggle");
+      var offset = $dropdown.offset();
+      var popoverTop = offset.top + $dropdown.outerHeight() + 15;
+
+      // if popoverTop lower then the height of the popover, popoverTop should be offset.top - popover height - 15
+      if (popoverTop + $popover.outerHeight() > $(window).height()) {
+        popoverTop = offset.top - $popover.outerHeight() - 15;
+        // also adjust the position of the popover triangle
+        $popover.find(".lui-popover__arrow").removeClass("lui-popover__arrow--top").addClass("lui-popover__arrow--bottom");
+      } else {
+        $popover.find(".lui-popover__arrow").removeClass("lui-popover__arrow--bottom").addClass("lui-popover__arrow--top");
+      }
+
+      // define "left" position to center the popover to the dropdown
+      var popoverLeft = offset.left + (($dropdown.outerWidth() - $popover.outerWidth())/2);
+
+      // if popoverLeft is negative, popoverLeft should be 10
+      if (popoverLeft < 0) {
+        console.log("popoverLeft is negative");
+        popoverLeft = 10;
+        // also adjust the position of the popover arrow
+        $popover.find(".lui-popover__arrow").css("left", "25%");
+      } else {
+        // if popover width + dropdownOffset is bigger then screen width - popoverLeft shpould be screen width - popover width + 10
+        if (popoverLeft + $popover.outerWidth() > $(window).width()) {
+          popoverLeft = $(window).width() - $popover.outerWidth() - 10;
+          // also adjust the position of the popover arrow
+          $popover.find(".lui-popover__arrow").css("left", "75%");
         } else {
-          popover.addClass("active");
+          $popover.find(".lui-popover__arrow").css("left", "50%");
         }
-      };
+      }
+
+      $(document.body)
+        .find(".listbox")
+        .each((_, item) => {
+          !$(item).is($popover) ? $(item).removeClass("active") : "";
+        });
+
+      if ($popover.hasClass("active")) {
+        $popover.removeClass("active");
+      } else {
+        $popover.css({
+          top: popoverTop,
+          left: popoverLeft,
+        });
+        $popover.addClass("active");
+      }
+
     };
     //Visible SelectionsMenu
     $scope.IsVisibleSelectionsMenuItems = false;
